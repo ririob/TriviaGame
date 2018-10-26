a9f36a2aac539cf1d574906d91fe05ddaae67e79
Richard Obaseki <obasekirichard8@gmail.com>
2018-10-26 12:20:27 +0100

    Adds results function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,43 +6,53 @@ var mainDiv = $("#xcard");
 var triviaGame = [{
             question: "When did Nigeria gain independence?",
             options: ["1940","1960","1950","1970","1961"],
-            rightOption: "1960"
+            rightOption: "1960",
+            image: "assets/images/1960.gif"
         }, {
             question: "Which countries colonized South Africa?",
             options: ["Portugal and France","Britain and France","Netherland and Britain","France and Netherland","Portugal and Britain"],
-            rightOption: "Netherland and Britain"
+            rightOption: "Netherland and Britain",
+            image: "assets/images/dutchbritain.gif"
         }, {
             question: "Who designed the Nigeria flag?",
             options: ["Taiwo Akinkunmi","Nnamdi Azikiwe","Olusegun Obasanjo","Mary Slessor","Benedict E. Odiase"],
-            rightOption: "Taiwo Akinkunmi"
+            rightOption: "Taiwo Akinkunmi",
+            image: "assets/images/9ja flag.gif"
         }, {
             question: "Which is the longest river in Africa?",
             options: ["Congo river","The Nile river","Zambezi","Limpopo","Blue Nile"],
-            rightOption: "The Nile river"
+            rightOption: "The Nile river",
+            image: "assets/images/nileriver.jpg"
         }, {
             question: "Which is the largest country in Africa?",
             options: ["South Africa","Egypt","Sudan","Libya","Algeria"],
-            rightOption: "Algeria"
+            rightOption: "Algeria",
+            image: "assets/images/malgeria.gif"
         }, {
             question: "How many states are there in Nigeria?",
             options: ["40","35","28","41","36"],
-            rightOption: "36"
+            rightOption: "36",
+            image: "assets/images/36-States-of-Nigeria.jpg"
         }, {
             question: "What year did President Umaru Musa Yar'adua die in office?",
             options: ["2009","2004","2010","2013","2011"],
-            rightOption: "2010"
+            rightOption: "2010",
+            image: "assets/images/presidentmusa.jpg"
         }, {
             question: "Which is the capital of the USA?",
             options: ["New York","California","Las Vegas","Washginton D.C","Texas"],
-            rightOption: "Washginton D.C"
+            rightOption: "Washginton D.C",
+            image: "assets/images/washgintondc.jpg"
         }, {
-            question: "Which is the executive capital of South Africa?",
+            question: "Which is the administrative capital of South Africa?",
             options: ["Pretoria","Cape Town","Johannesburg","Bloemfontein","Durban"],
-            rightOption: "Pretoria"
+            rightOption: "Pretoria",
+            image: "assets/images/pretoriasa.jpg"
         }, {
-            question: "Which state is Nollywood based in Nigeria?",
-            options: ["Edo","Lagos","Abuja","Cross river","Rivers"],
-            rightOption: "Cross river"
+            question: "Which country won the 2018 FIFA world cup?",
+            options: ["Argentina","England","Spain","France","Germany"],
+            rightOption: "France",
+            image: "assets/images/france2018.jpg"
         }];
 
 // Variable to hold the setInterval
@@ -97,7 +107,7 @@ function timeUp() {
     // Clearinng our timer
     clearInterval(timer);
     // Using jQuery to append the counter to to the counnter div
-    $("#scard").html(counter);
+    $("#scard").text(counter);
     // Message to show when time is up
     mainDiv.html("<h4>Time's Up!</h4>");
     mainDiv.append("<h5>The Correct Answer was: " + questions[this.currentQuestion].rightOption);
@@ -109,7 +119,23 @@ function timeUp() {
     else {
         // show the next question
         setTimeout(nextQuestion, 3 * 1000);
-    }
+    };
+
+    // Creating the results disply function
+    function results() {
+        // clearing the timer
+        clearInterval(timer);
+        // Displaying results(appending to question div)
+        mainDiv.html("<h5>All done, here's how you did!");
+        // appending counter to counter div(using jQuery of course)
+        $("#scard").text(counter);
+        // Still display results
+        mainDiv.append("<h6>Correct Answer: " + correct + "</h6>");
+        mainDiv.append("<h6>Wrong Answers: " + incorrect + "</h6>");
+        mainDiv.append("<h6>Unanswered: " + (questions.length - (incorrect + correct)) + "</h6>");
+        // Creating a button to start over
+        mainDiv.append("<br><button class=\"btn btn-primary\" id=\"start-over\">Restart?</button>");
+    };
 };
 
 
